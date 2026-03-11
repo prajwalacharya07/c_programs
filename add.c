4a05177a3817e0e898e9aa0579c398e88a1fd21a
prajwalacharya07 <aprajwal138@gmail.com>
2026-03-11 20:35:21 +0530

    add 2 in c

---
+++ add.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+   //program to add two numbers
+   int a,b,sum;
+   printf("Enter two numbers :");
+   scanf("%d %d",&a,&b);
+   sum = a + b;
+   printf("\n Sum of the two numbers :%d",sum);
+    return 0;
+}
